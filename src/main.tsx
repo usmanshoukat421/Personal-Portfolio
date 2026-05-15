55e5abc9ff049348117d61480c1355a97a9a2828
Usman Shoukat <usmandotshoukat@gmail.com>
2026-05-15 20:40:22 +0500

    fix: working production build for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css";
-import App from "./App";
-
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./router";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
